132bf4feef4cda314b66cc3d5ca09a683f4d0429
Leandro Aranda <learanda@hotmail.com.ar>
2021-11-20 12:31:16 -0300

    I add the initial project

---
+++ app/src/main/java/ar/leandro/meliexplorer/ui/MainActivity.kt
@@ -0,0 +1,24 @@
+package ar.leandro.meliexplorer.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ar.leandro.meliexplorer.R
+import ar.leandro.meliexplorer.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        hola()
+    }
+
+    private fun hola() {
+        binding.lan.text = getString(R.string.hello_lan)
+    }
+}
