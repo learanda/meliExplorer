349be47e1517f422a537a4a21f66ac77c2c43a8a
Leandro Aranda <learanda@hotmail.com.ar>
2021-11-29 14:57:52 -0300

    I set listener and observer on 'SearchActivity'

--- app/src/main/java/ar/leandro/meliexplorer/ui/SearchActivity.kt
+++ app/src/main/java/ar/leandro/meliexplorer/ui/SearchActivity.kt
@@ -4,10 +4,14 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import ar.leandro.meliexplorer.databinding.ActivitySearchBinding
+import ar.leandro.meliexplorer.domain.ItemArticlesAdapter
+import ar.leandro.meliexplorer.ui.viewmodels.MainViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class SearchActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySearchBinding
+    private val vm: MainViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,6 +19,22 @@ class SearchActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        setListeners()
+        setObservers()
+    }
+
+    private fun setListeners() {
+        binding.btnSearch.setOnClickListener {
+            vm.findArticles(binding.etSearch.text.toString())
+        }
+    }
+
+    private fun setObservers() {
+        vm.articlesList.observe(this, {
+            if (it != null) {
+                binding.rvArticles.adapter = ItemArticlesAdapter(it)
+            }
+        })
     }
 
     override fun onBackPressed() {
