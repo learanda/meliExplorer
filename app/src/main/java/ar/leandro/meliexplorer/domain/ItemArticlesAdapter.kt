80cae304d47727f4ebb0432fa5e2f8401efd2e19
Leandro Aranda <learanda@hotmail.com.ar>
2021-12-03 21:12:08 -0300

    Addition of notifyDataSetChanged

--- app/src/main/java/ar/leandro/meliexplorer/domain/ItemArticlesAdapter.kt
+++ app/src/main/java/ar/leandro/meliexplorer/domain/ItemArticlesAdapter.kt
@@ -3,19 +3,22 @@ package ar.leandro.meliexplorer.domain
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import ar.leandro.meliexplorer.R
 import ar.leandro.meliexplorer.databinding.ItemArticleBinding
 import ar.leandro.meliexplorer.domain.model.Article
 import com.squareup.picasso.Picasso
 
 
-class ItemArticlesAdapter(val articles: List<Article>) : RecyclerView.Adapter<ItemArticlesAdapter.ItemArticlesHolder>() {
+class ItemArticlesAdapter(var articles: List<Article>) : RecyclerView.Adapter<ItemArticlesAdapter.ItemArticlesHolder>() {
 
     class ItemArticlesHolder(val binding: ItemArticleBinding) : RecyclerView.ViewHolder(binding.root) {
 
         fun bind(articles: Article) {
             binding.tvTitle.text = articles.title
             binding.tvPrice.text = articles.price.toString()
-            Picasso.get().load(articles.thumbnail).into(binding.ivPicture)
+            Picasso.get()
+                .load(articles.thumbnail)
+                .into(binding.ivPicture)
         }
     }
 
@@ -30,4 +33,10 @@ class ItemArticlesAdapter(val articles: List<Article>) : RecyclerView.Adapter<It
     }
 
     override fun getItemCount(): Int = articles.size
+
+    fun setListData(data: List<Article>) {
+        this.articles = data
+        this.notifyDataSetChanged()
+
+    }
 }
